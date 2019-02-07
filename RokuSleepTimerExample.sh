3e7d97ed1931601cb31c9144714892a70f38955d
hercengkody <44627286+hercengkody@users.noreply.github.com>
2019-02-07 12:22:09 -0600

    Update RokuSleepTimerExample.sh

--- RokuSleepTimerExample.sh
+++ RokuSleepTimerExample.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-#ask for sleep timer lenght
-
+#ask for sleep timer length
 echo How long?
 
+#input timer length using following format.
+#Time in seconds, XX
+#Time in minutes, XXm
+#Time in hours, XXh
 read vartime
 
+#repeats how long the timer was set for
 echo Timer set for $vartime
 
-sleep $vartime ; curl -d ' ' http://192.168.0.35:8060/keypress/play
-
+#waits for length of time set by user then sends a "keypress/play"
+#command to the specified Roku, this will pause whatever show is playing
+sleep $vartime ; curl -d ' ' http://ROKU_IP_HERE:8060/keypress/play
